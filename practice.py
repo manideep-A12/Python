9613504dd12b5f03ccdc6ec392b45012cdafa91b
Manideep <130486317+manideep420@users.noreply.github.com>
2025-07-18 12:25:49 +0530

    am done

--- practice.py
+++ practice.py
@@ -1,10 +1,4 @@
-n=int(input("ENter :"))
-is_prime=False
-for i in range(2,int(n**0.5)+1):
-  if(n%i==0):
-    is_prime=True
-    break
-if is_prime:
-  print("prime")
-else:
-  print("non-prime")
+my=('mani','mourya','vishnu')
+m=iter(my)
+for i in range(len(my)):
+  print(next(m))
